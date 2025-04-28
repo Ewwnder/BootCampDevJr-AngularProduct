d69f8b1c5a58883c893614a981e6b888aabc9230
Nicolas Ferreira <nicolas.as.ferreira@hotmail.com>
2025-04-28 17:42:20 -0300

    Load Categories

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Category } from '../../interfaces/Category';
 
 @Component({
   selector: 'app-products',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+
+  categories : Category[] = [
+    {id:1, name:"Produção Própria"},
+    {id:2, name:"Nacional"},
+    {id:3, name:"Importado"},
+    {id:4, name:"Premium"},
+  ];
 
 }
