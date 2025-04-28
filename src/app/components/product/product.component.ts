60e74235fb7add447f1b27d6258339fad44ae73b
Nicolas Ferreira <nicolas.as.ferreira@hotmail.com>
2025-04-28 18:11:02 -0300

    Register Button

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Category } from '../../interfaces/Category';
+import { Product } from '../../interfaces/Product';
 
 @Component({
   selector: 'app-product',
@@ -11,5 +12,12 @@ export class ProductComponent {
 
   @Input()
   categories : Category[] = [];
+
+  
+  product : Product = {} as Product;
+
+  save(){
+    console.log(this.product);
+  }
 
 }
