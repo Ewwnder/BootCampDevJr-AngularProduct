8dafbbc51d2410c67fb99f58f0d2ff1658758dad
Nicolas Ferreira <nicolas.as.ferreira@hotmail.com>
2025-04-28 19:09:51 -0300

    Added Services

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Category } from '../../interfaces/Category';
 import { Product } from '../../interfaces/Product';
+import { CategoryService } from '../../services/category.service';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-products',
@@ -8,24 +10,23 @@ import { Product } from '../../interfaces/Product';
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
-  categories : Category[] = [
-    {id:1, name:"Produção Própria"},
-    {id:2, name:"Nacional"},
-    {id:3, name:"Importado"},
-    {id:4, name:"Premium"},
-  ];
+  categories : Category[] = [];
 
   product : Product = {} as Product;
   products : Product[] = [];
 
-  saveProduct(){
-    this.product.id = this.products.length+1;
-    this.products.push(this.product);
-    this.product = {} as Product;
+  constructor(private categoryService: CategoryService, private productService:ProductService) { }   //Injetando esse objeto dentro do componente. Para injetar o serviço utilizamos o constructor
 
-    console.log("Novo produto cadastrado. Total de produtos: " + this.products.length);
+  ngOnInit(): void{
+    this.categories = this.categoryService.getCategories();
+    this.products = this.productService.getProducts();
+  }
+
+  saveProduct(){
+    this.productService.save(this.product);
+    this.product = {} as Product;
   }
 
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../interfaces/Product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+
+  products : Product[] = [];
+  constructor() { }
+
+  getProducts() : Product[]{   //Recupera todos os produtos
+    return this.products;
+  }
+
+  save(product:Product){
+    product.id = this.products.length+1;
+    this.products.push(product);
+  }
+}
+
